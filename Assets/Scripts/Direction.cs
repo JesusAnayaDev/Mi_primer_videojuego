8958cfadb5af67f3251e11200346f57505eb748f
JesusAnayaDev <jesusanaya558@gmail.com>
2022-05-29 15:22:32 -0500

    Se agrega el proyecto

---
+++ Assets/Scripts/Direction.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Direction : MonoBehaviour
+{
+    private Rigidbody2D _rigidbody;
+    public float speed = 2f;
+
+    void Awake()
+	{
+		_rigidbody = GetComponent<Rigidbody2D>();
+	}
+
+    public Vector2 direction;
+
+    private void FixedUpdate()
+	{
+		//  Move object
+		Vector2 movement = direction.normalized * speed;
+		_rigidbody.velocity = movement;
+	}
+}
---
+++ Assets/Scripts/Pausar.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Pausar : MonoBehaviour
+{
+    [SerializeField] private GameObject botonPausa;
+    [SerializeField] private GameObject menuPausa;
+
+    public void Pausa(){
+        Time.timeScale = 0f;
+        botonPausa.SetActive(false);
+        menuPausa.SetActive(true);
+    }
+
+    public void Reaundar(){
+        Time.timeScale = 1f;
+        botonPausa.SetActive(true);
+        menuPausa.SetActive(false);
+    }
+
+    public void Reiniciar(){
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void Cerrar(){
+        Debug.Log("Cerrando Juego");
+        Application.Quit();
+    }
+
+    public void Menu(){
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("StartMenu");
+    }
+}
